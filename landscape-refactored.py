34d2f392024926eddfc6a9e3bb0e40f5c90328da
LucasM212 <43610120+LucasM212@users.noreply.github.com>
2018-11-14 09:28:26 -0500

    Create landscape-refactored.py

---
+++ landscape-refactored.py
@@ -0,0 +1,34 @@
+def setup():
+    size(640, 480)
+    
+def draw():
+  background(139, 90, 0)
+  draw_sun(320, -100)
+  draw_cloud1(-100, height)
+  draw_cloud2(640, 240)
+
+def draw_sun(x, height2):
+  if height2 >= 640:
+     height2 = -100
+  height2 += 1
+  noStroke()
+  fill(255,255,0)
+  ellipse(x, height2, 100, 100)
+
+def cloud1(x2, height):
+  if x2 >= 800:
+        x2 = -100
+  x2 += 1
+  fill(255, 255, 255)
+  ellipse(x2, height/2, 50, 50)
+  ellipse(x2+30, height/2, 50, 50)
+  ellipse(x2+10, height/2-20, 50, 50)
+
+def cloud2(x3, height3):
+  if x3 <= -100:
+      x3 = 640
+  x3 -= 1
+  fill(255, 255, 255)
+  ellipse(x3, height3, 50, 50)
+  ellipse(x3+30, height3, 50, 50)
+  ellipse(x3+10, height3-20, 50, 50)
